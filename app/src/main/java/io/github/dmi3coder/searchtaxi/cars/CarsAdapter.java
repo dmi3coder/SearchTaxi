ad0eb119d42c52e5b98af316052fda13197c4ad3
dmi3coder <dmi3coder@gmail.com>
2017-08-26 16:19:42 +0300

    Binding taxi data to layout

--- app/src/main/java/io/github/dmi3coder/searchtaxi/cars/CarsAdapter.java
+++ app/src/main/java/io/github/dmi3coder/searchtaxi/cars/CarsAdapter.java
@@ -1,12 +1,13 @@
 package io.github.dmi3coder.searchtaxi.cars;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
 import io.github.dmi3coder.searchtaxi.R;
 import io.github.dmi3coder.searchtaxi.cars.CarsAdapter.CarHolder;
 import io.github.dmi3coder.searchtaxi.data.Taxi;
+import io.github.dmi3coder.searchtaxi.databinding.ItemCarBinding;
 import java.util.List;
 
 /**
@@ -23,15 +24,31 @@ public class CarsAdapter extends RecyclerView.Adapter<CarHolder> {
   @Override
   public CarHolder onCreateViewHolder(ViewGroup parent, int viewType) {
     return new CarHolder(
-        LayoutInflater.from(parent.getContext())
-            .inflate(R.layout.item_car, parent, false)
-    );
+        ItemCarBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
   }
 
   @Override
   public void onBindViewHolder(CarHolder holder, int position) {
-
+    Taxi car = cars.get(position);
+    ItemCarBinding binding = holder.binding;
+    binding.name.setText(car.getName());
+    binding.location.setText(car.getAddress());
+    binding.fuelProgress.setProgress(car.getFuel());
+    Context context = holder.itemView.getContext();
+    binding.interiorTitle.setText(context
+        .getString(
+            R.string.interior_prefixed,
+            context.getString(car.getInterior().getStringRes())
+        )
+    );
+    binding.exteriorTitle.setText(context
+        .getString(
+            R.string.exterior_prefixed,
+            context.getString(car.getExterior().getStringRes())
+        )
+    );
   }
+
 
   @Override
   public int getItemCount() {
@@ -40,8 +57,11 @@ public class CarsAdapter extends RecyclerView.Adapter<CarHolder> {
 
   public class CarHolder extends RecyclerView.ViewHolder {
 
-    public CarHolder(View itemView) {
-      super(itemView);
+    private ItemCarBinding binding;
+
+    public CarHolder(ItemCarBinding binding) {
+      super(binding.getRoot());
+      this.binding = binding;
     }
   }
 }
--- app/src/main/java/io/github/dmi3coder/searchtaxi/data/ComparisonMark.java
+++ app/src/main/java/io/github/dmi3coder/searchtaxi/data/ComparisonMark.java
@@ -1,8 +1,22 @@
 package io.github.dmi3coder.searchtaxi.data;
 
+import android.support.annotation.StringRes;
+import io.github.dmi3coder.searchtaxi.R;
+
 /**
  * Created by dim3coder on 8/26/17.
  */
 public enum ComparisonMark {
-  GOOD, UNACCEPTABLE
+  GOOD(R.string.good_mark), UNACCEPTABLE(R.string.unacceptable_mark);
+
+  @StringRes int stringRes;
+
+  ComparisonMark(int stringRes) {
+    this.stringRes = stringRes;
+  }
+
+  public @StringRes
+  int getStringRes() {
+    return stringRes;
+  }
 }
